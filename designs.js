3d268272accfabf8a8181f7dbf3fb82db24a3974
Annija-Cilinska <annija333@gmail.com>
2018-04-11 15:11:29 +0300

    fixed

--- designs.js
+++ designs.js
@@ -26,23 +26,7 @@ function makeGrid() {
         }
     }
 
-    for (let x = 0; x < rows; x++) {
-        const row = table.insertRow(x);
-        row.setAttribute("class", "row");
-        for (let y = 0; y < columns; y++) {
-            const cell = row.insertCell(y);
-            cell.addEventListener('click', function (evt) {
-                evt.target.style.backgroundColor = backgroundColor;
-
-            });
-
-        }
-    }
-
-
-
-
-    return false;
+   return false;
 }
 document.getElementById("sizePicker").addEventListener("submit", function (evt) {
     evt.preventDefault();
