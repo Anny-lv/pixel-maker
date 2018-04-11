9de10d13c15fc815d1990107e1715f7be79e59a9
Annija-Cilinska <annija333@gmail.com>
2018-04-11 15:00:56 +0300

    updated html and js file

--- designs.js
+++ designs.js
@@ -4,7 +4,30 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    const table = document.getElementById("pixelCanvas");
+    table.innerHTML = "";
 
-// Your code goes here!
 
+    while (table.rows.length > 1)
+        table.deleteRow(0);
+    const rows = document.getElementById("inputHeight").value;
+    const columns = document.getElementById("inputWidth").value;
+
+    for (let x = 0; x < rows; x++) {
+        const row = table.insertRow(x);
+        row.setAttribute("class", "row");
+        for (let y = 0; y < columns; y++) {
+            const cell = row.insertCell(y);
+            cell.addEventListener('mouseover', function (evt) {
+                evt.target.style.backgroundColor = document.getElementById("colorPicker").value;
+
+            }
+            );
+        }
+    }
+    return false;
 }
+document.getElementById("sizePicker").addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    makeGrid();
+});
