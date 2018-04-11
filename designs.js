28da49f535bf18ed42b6ac102902c3a43d5ecd73
Annija-Cilinska <annija333@gmail.com>
2018-04-11 15:09:58 +0300

    onmouseover changet to click

--- designs.js
+++ designs.js
@@ -18,13 +18,30 @@ function makeGrid() {
         row.setAttribute("class", "row");
         for (let y = 0; y < columns; y++) {
             const cell = row.insertCell(y);
-            cell.addEventListener('mouseover', function (evt) {
+            cell.addEventListener('click', function (evt) {
                 evt.target.style.backgroundColor = document.getElementById("colorPicker").value;
 
-            }
-            );
+            });
+
         }
     }
+
+    for (let x = 0; x < rows; x++) {
+        const row = table.insertRow(x);
+        row.setAttribute("class", "row");
+        for (let y = 0; y < columns; y++) {
+            const cell = row.insertCell(y);
+            cell.addEventListener('click', function (evt) {
+                evt.target.style.backgroundColor = backgroundColor;
+
+            });
+
+        }
+    }
+
+
+
+
     return false;
 }
 document.getElementById("sizePicker").addEventListener("submit", function (evt) {
